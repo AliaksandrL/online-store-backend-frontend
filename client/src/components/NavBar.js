3cc6cf059726c61d97bbe1f2792735259a999831
AliaksandrL <lipskialiaksandr@gmail.com>
2022-05-29 19:03:00 +0300

    Page for viewing the product and its characteristics

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -2,19 +2,32 @@ import React, {useContext} from 'react';
 import {Context} from "../index";
 import {Button, Container, Nav, Navbar} from "react-bootstrap";
 import {NavLink} from "react-router-dom";
-import {SHOP_ROUTE} from "../utils/consts";
+import {ADMIN_ROUTE, LOGIN_ROUTE, SHOP_ROUTE} from "../utils/consts";
 import {observer} from "mobx-react-lite";
+import {useHistory} from "react-router-dom";
 
 const NavBar = observer(() => {
     const {user} = useContext(Context)
+    const history = useHistory()
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
                 <NavLink style={{color: 'white'}} to={SHOP_ROUTE}>ShopDevice</NavLink>
                 {user.isAuth ?
                     <Nav className="ml-auto" style={{color: "white"}}>
-                        <Button variant={"outline-light"}>Админ панель</Button>
-                        <Button variant={"outline-light"} className="ms-4">Войти</Button>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => history.push(ADMIN_ROUTE)}
+                        >
+                            Админ панель
+                        </Button>
+                        <Button
+                            variant={"outline-light"}
+                            onClick={() => history.push(LOGIN_ROUTE)}
+                            className="ms-4"
+                        >
+                            Выйти
+                        </Button>
                     </Nav>
                     :
                     <Nav className="ml-auto" style={{color: "white"}}>
--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,10 +1,64 @@
 import React from 'react';
+import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
+import bigStar from '../assets/star.png'
 
 const DevicePage = () => {
+    const device = {
+        id: 1,
+        name: 'Iphone 13',
+        price: 3000,
+        rating: 5,
+        img: 'https://c.neh.tw/thumb/f/720/bd6826b2e6d742e39fa1.jpg'
+    }
+    const description = [
+        {id:1, title: 'Оперативная память', description: '5 гб'},
+        {id:2, title: 'Камера', description: '5 мп'},
+        {id:3, title: 'Процессор', description: 'Core i7'},
+        {id:4, title: 'Кол-во ядер', description: '2'},
+        {id:5, title: 'Аккумулятор', description: '4000'},
+    ]
     return (
-        <div>
-            DevicePage
-        </div>
+        <Container className="mt-3">
+            <Row>
+                <Col md={4}>
+                    <Image width={300} height={300} src={device.img}/>
+                </Col>
+                <Col md={4}>
+                    <Row className="d-flex flex-column align-items-center">
+                        <h2>{device.name}</h2>
+                        <div
+                            className="d-flex align-items-center justify-content-center"
+                            style={{
+                                background: `url(${bigStar}) no-repeat center center`,
+                                width: 240,
+                                height: 240,
+                                backgroundSize: 'cover',
+                                fontSize: 64
+                            }}
+                        >
+                            {device.rating}
+                        </div>
+                    </Row>
+                </Col>
+                <Col md={4}>
+                    <Card
+                        className="d-flex flex-column align-items-center justify-content-around"
+                        stule={{width: 300, height: 300, fontSize: 32, border: '5px solid lightgray'}}
+                    >
+                        <h3>От: {device.price} руб.</h3>
+                        <Button variant={"outline-dark"}>Добавить в корзину</Button>
+                    </Card>
+                </Col>
+            </Row>
+            <Row className="d-flex flex-column mt-3">
+                <h1>Характеристики</h1>
+                {description.map((info, index) =>
+                    <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
+                        {info.title}: {info.description}
+                    </Row>
+                )}
+            </Row>
+        </Container>
     );
 };
 
