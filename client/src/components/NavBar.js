7da1b9edf0147e72c9d1733568308a5f2f1cefa5
AliaksandrL <lipskialiaksandr@gmail.com>
2022-08-08 16:55:40 +0300

    Server interaction. Setting up axios

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -9,6 +9,12 @@ import {useHistory} from "react-router-dom";
 const NavBar = observer(() => {
     const {user} = useContext(Context)
     const history = useHistory()
+
+    const logOut = () => {
+        user.setUser({})
+        user.setIsAuth(false)
+    }
+
     return (
         <Navbar bg="dark" variant="dark">
             <Container>
@@ -23,7 +29,7 @@ const NavBar = observer(() => {
                         </Button>
                         <Button
                             variant={"outline-light"}
-                            onClick={() => history.push(LOGIN_ROUTE)}
+                            onClick={() => logOut()}
                             className="ms-4"
                         >
                             Выйти
@@ -31,7 +37,7 @@ const NavBar = observer(() => {
                     </Nav>
                     :
                     <Nav className="ml-auto" style={{color: "white"}}>
-                        <Button variant={"outline-light"} onClick={() => user.setIsAuth(true)}>Авторизация</Button>
+                        <Button variant={"outline-light"} onClick={() => history.push(LOGIN_ROUTE)}>Авторизация</Button>
                     </Nav>
                 }
             </Container>
--- client/src/pages/DevicePage.js
+++ client/src/pages/DevicePage.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {Button, Card, Col, Container, Image, Row} from "react-bootstrap";
 import bigStar from '../assets/star.png'
+import {useParams} from "react-router-dom";
+import {fetchDevices} from "../http/deviceAPI";
 
 const DevicePage = () => {
-    const device = {
-        id: 1,
-        name: 'Iphone 13',
-        price: 3000,
-        rating: 5,
-        img: 'https://c.neh.tw/thumb/f/720/bd6826b2e6d742e39fa1.jpg'
-    }
-    const description = [
-        {id:1, title: 'Оперативная память', description: '5 гб'},
-        {id:2, title: 'Камера', description: '5 мп'},
-        {id:3, title: 'Процессор', description: 'Core i7'},
-        {id:4, title: 'Кол-во ядер', description: '2'},
-        {id:5, title: 'Аккумулятор', description: '4000'},
-    ]
+    const [device, setDevice] = useState({info: []})
+    const {id} = useParams()
+
+    useEffect(() => {
+        fetchDevices(id).then(data => setDevice(data))
+    }, [])
+
+    // const device = {
+    //     id: 1,
+    //     name: 'Iphone 13',
+    //     price: 3000,
+    //     rating: 5,
+    //     img: 'https://c.neh.tw/thumb/f/720/bd6826b2e6d742e39fa1.jpg'
+    // }
+    // const description = [
+    //     {id:1, title: 'Оперативная память', description: '5 гб'},
+    //     {id:2, title: 'Камера', description: '5 мп'},
+    //     {id:3, title: 'Процессор', description: 'Core i7'},
+    //     {id:4, title: 'Кол-во ядер', description: '2'},
+    //     {id:5, title: 'Аккумулятор', description: '4000'},
+    // ]
     return (
         <Container className="mt-3">
             <Row>
@@ -52,7 +61,7 @@ const DevicePage = () => {
             </Row>
             <Row className="d-flex flex-column mt-3">
                 <h1>Характеристики</h1>
-                {description.map((info, index) =>
+                {device.info.map((info, index) =>
                     <Row key={info.id} style={{background: index % 2 === 0 ? 'lightgray' : 'transparent', padding: 10}}>
                         {info.title}: {info.description}
                     </Row>
