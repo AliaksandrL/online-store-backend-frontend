6328dd8093b29e0c928fadd927ec6c678cd00d85
AliaksandrL <lipskialiaksandr@gmail.com>
2022-08-11 21:28:48 +0300

    Adding Devices and Features

--- client/src/components/modals/CreateDevice.js
+++ client/src/components/modals/CreateDevice.js
@@ -1,17 +1,36 @@
-import React, {useContext, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import {Button, Col, Dropdown, Form, Modal, Row} from "react-bootstrap";
 import {Context} from "../../index";
 import DropdownMenu from "react-bootstrap/DropdownMenu";
+import {fetchBrands, fetchTypes} from "../../http/deviceAPI";
+import {observer} from "mobx-react-lite";
 
-const CreateDevice = ({show, onHide}) => {
+const CreateDevice = observer(({show, onHide}) => {
     const {device} = useContext(Context)
+    const [name, setName] = useState('')
+    const [price, setPrice] = useState(0)
+    const [file, setFile] = useState(null)
     const [info, setInfo] = useState([])
+
+    useEffect(() => {
+        fetchTypes().then(data => device.setTypes(data))
+        fetchBrands().then(data => device.setBrands(data))
+    }, [])
 
     const addInfo = () => {
         setInfo([...info, {title: '', description: '', number: Date.now()}])
     }
     const removeInfo = (number) => {
         setInfo(info.filter(i => i.number !== number))
+    }
+    const changeInfo = (key, value, number) => {
+        setInfo(info.map(i => i.number === number ? {...i, [key]: value} : i))
+    }
+    const selectFile = e => {
+        setFile(e.target.files[0])
+    }
+    const addDevice = () => {
+
     }
 
     return (
@@ -29,26 +48,30 @@ const CreateDevice = ({show, onHide}) => {
             <Modal.Body>
                 <Form>
                     <Dropdown className="mt-2 md-2">
-                        <Dropdown.Toggle>Выберите тип</Dropdown.Toggle>
+                        <Dropdown.Toggle>{device.selectedType.name || "Выберите тип"}</Dropdown.Toggle>
                         <DropdownMenu>
                             {device.types.map(type =>
-                                <Dropdown.Item key={type.id}>{type.name}</Dropdown.Item>
+                                <Dropdown.Item onClick={() => device.setSelectedType(type)} key={type.id}>{type.name}</Dropdown.Item>
                             )}
                         </DropdownMenu>
                     </Dropdown>
                     <Dropdown className="mt-2 md-2">
-                        <Dropdown.Toggle>Выберите брэнд</Dropdown.Toggle>
+                        <Dropdown.Toggle>{device.selectedBrand.name || "Выберите брэнд"}</Dropdown.Toggle>
                         <DropdownMenu>
                             {device.brands.map(brand =>
-                                <Dropdown.Item key={brand.id}>{brand.name}</Dropdown.Item>
+                                <Dropdown.Item onClick={() => device.setSelectedBrand(brand)} key={brand.id}>{brand.name}</Dropdown.Item>
                             )}
                         </DropdownMenu>
                     </Dropdown>
                     <Form.Control
+                        value={name}
+                        onChange={e => setName(e.target.value)}
                         className="mt-3"
                         placeholder="Введите название устройства"
                     />
                     <Form.Control
+                        value={price}
+                        onChange={e => setPrice(Number(e.target.value))}
                         className="mt-3"
                         placeholder="Введите стоимость устройства"
                         type="number"
@@ -56,6 +79,7 @@ const CreateDevice = ({show, onHide}) => {
                     <Form.Control
                         className="mt-3"
                         type="file"
+                        onChange={selectFile}
                     />
                     <hr/>
                     <Button
@@ -68,12 +92,16 @@ const CreateDevice = ({show, onHide}) => {
                         <Row className="mt-4" key={i.number}>
                             <Col md={4}>
                                 <Form.Control
+                                    value={i.title}
+                                    onChange={(e) => changeInfo('title', e.target.value, i.number)}
                                     placeholder="Введите название свойства"
                                 />
                             </Col>
                             <Col md={4}>
                                 <Form.Control
-                                    placeholder="Введите название свойства"
+                                    value={i.description}
+                                    onChange={(e) => changeInfo('description', e.target.value, i.number)}
+                                    placeholder="Введите описание свойства"
                                 />
                             </Col>
                             <Col md={4}>
@@ -90,10 +118,10 @@ const CreateDevice = ({show, onHide}) => {
             </Modal.Body>
             <Modal.Footer>
                 <Button variant="outline-danger" onClick={onHide}>Закрыть</Button>
-                <Button variant="outline-success" onClick={onHide}>Добавить</Button>
+                <Button variant="outline-success" onClick={addDevice}>Добавить</Button>
             </Modal.Footer>
         </Modal>
     );
-};
+});
 
 export default CreateDevice;
--- client/src/store/DeviceStore.js
+++ client/src/store/DeviceStore.js
@@ -15,15 +15,49 @@ export default class DeviceStore {
             {id: 4, name: 'Lenovo'},
         ]
         this._devices = [
-            {id: 1, name: 'Iphone 13', price: 3000, rating: 5, img: 'https://c.neh.tw/thumb/f/720/bd6826b2e6d742e39fa1.jpg'},
-            {id: 2, name: 'Iphone 13', price: 3000, rating: 5, img: 'https://c.neh.tw/thumb/f/720/bd6826b2e6d742e39fa1.jpg'},
-            {id: 3, name: 'Iphone 13', price: 3000, rating: 5, img: 'https://c.neh.tw/thumb/f/720/bd6826b2e6d742e39fa1.jpg'},
-            {id: 4, name: 'Iphone 13', price: 3000, rating: 5, img: 'https://c.neh.tw/thumb/f/720/bd6826b2e6d742e39fa1.jpg'},
-            {id: 5, name: 'Iphone 13', price: 3000, rating: 5, img: 'https://c.neh.tw/thumb/f/720/bd6826b2e6d742e39fa1.jpg'},
-            {id: 6, name: 'Iphone 13', price: 3000, rating: 5, img: 'https://c.neh.tw/thumb/f/720/bd6826b2e6d742e39fa1.jpg'},
+            {
+                id: 1,
+                name: 'Iphone 13',
+                price: 3000,
+                rating: 5,
+                img: 'https://c.neh.tw/thumb/f/720/bd6826b2e6d742e39fa1.jpg'
+            },
+            {
+                id: 2,
+                name: 'Iphone 13',
+                price: 3000,
+                rating: 5,
+                img: 'https://c.neh.tw/thumb/f/720/bd6826b2e6d742e39fa1.jpg'
+            },
+            {
+                id: 3,
+                name: 'Iphone 13',
+                price: 3000,
+                rating: 5,
+                img: 'https://c.neh.tw/thumb/f/720/bd6826b2e6d742e39fa1.jpg'
+            },
+            {
+                id: 4,
+                name: 'Iphone 13',
+                price: 3000,
+                rating: 5,
+                img: 'https://c.neh.tw/thumb/f/720/bd6826b2e6d742e39fa1.jpg'
+            },
+            {
+                id: 5,
+                name: 'Iphone 13',
+                price: 3000,
+                rating: 5,
+                img: 'https://c.neh.tw/thumb/f/720/bd6826b2e6d742e39fa1.jpg'
+            },
+            {
+                id: 6,
+                name: 'Iphone 13',
+                price: 3000,
+                rating: 5,
+                img: 'https://c.neh.tw/thumb/f/720/bd6826b2e6d742e39fa1.jpg'
+            },
         ]
-        this._selectedType = {}
-        this._selectedBrand = {}
         makeAutoObservable(this)
     }
 
@@ -40,8 +74,9 @@ export default class DeviceStore {
     }
 
     setSelectedType(type) {
-    this._selectedType = type
+        this._selectedType = type
     }
+
     setSelectedBrand(brand) {
         this._selectedBrand = brand
     }
@@ -53,12 +88,15 @@ export default class DeviceStore {
     get brands() {
         return this._brands
     }
+
     get devices() {
         return this._devices
     }
+
     get selectedType() {
         return this._selectedType
     }
+
     get selectedBrand() {
         return this._selectedBrand
     }
